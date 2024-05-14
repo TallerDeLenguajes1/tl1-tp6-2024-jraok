1dae26de084dc152d44e5b4fab151cacce544b5c
Joel Roman Arancibia <joelromanarancibia@gmail.com>
2024-05-14 13:21:23 -0300

    estructura del ejercicio 1

--- Program.cs
+++ Program.cs
@@ -6,3 +6,27 @@ a=10;
 b=a;
 Console.WriteLine("Valor de la variable A: "+a);
 Console.WriteLine("Valor de la variable B: "+b);
+
+string x;
+int y;
+int aux;
+
+Console.WriteLine("Ingrese el primer valor a invertir: ");
+do
+{
+    x=Console.ReadLine();
+    if (!int.TryParse(,))
+    {
+        Console.WriteLine("Valor no admitido, reingrese"); 
+    }    
+} while (int.TryParse());
+
+Console.WriteLine("Ingrese el segundo valor a invertir: ");
+
+{
+    y=Console.ReadLine();
+    if (!int.TryParse(,))
+    {
+        Console.WriteLine("Valor no admitido, reingrese"); 
+    }    
+} while (int.TryParse());
